98c29c585557bb059510d380bca291bd3d172937
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-20 15:39:15 +0500

    Place Middlewares in their respective places

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 
+const errorHandlerMiddleware = require("./middleware/error-handler.middleware");
+const notFoundMiddleware = require("./middleware/not-found.middleware");
+
 // Middleware
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 })
+
+
+// Error handling middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
