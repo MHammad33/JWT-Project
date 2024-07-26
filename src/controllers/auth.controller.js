08ba56c6298bf31c4c8e672cd11cf60347147452
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-26 21:48:13 +0500

    Add Login Logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,20 @@
+const jwt = require("jsonwebtoken");
+const CustomApiError = require("../errors/custom-error");
+
 const login = async (req, res) => {
   const { username, password } = req.body;
 
-  res.json({ msg: `Hello ${username}, you have successfully logged in` });
+  if (!username || !password) {
+    // 400 is a bad request
+    throw new CustomApiError("Username and Password Required", 400);
+  }
+
+  // Token Creation
+  const id = new Date().getDate();
+  const userForToken = { username, id };
+  const token = jwt.sign(userForToken, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
+
+  res.json({ msg: "User created", token });
 }
 
 const dashboard = async (req, res) => {
