3d2175481f934f146d8afb0c8e9c655075bbe3fd
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-26 22:10:30 +0500

    Fix file error

--- src/middleware/error-handler.middleware.js
+++ src/middleware/error-handler.middleware.js
@@ -1,5 +1,10 @@
-const notFound = (req, res) => {
-  return res.status(404).json({ message: "Route does not exist" });
+const CustomApiError = require("../errors/custom-error");
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomApiError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  return res.status(500).json({ message: "Something went wrong, please try again" });
 }
 
-module.exports = notFound;
+module.exports = errorHandlerMiddleware;
--- src/middleware/not-found.middleware.js
+++ src/middleware/not-found.middleware.js
@@ -1,10 +1,5 @@
-const CustomApiError = require("../errors/custom-error");
-
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomApiError) {
-    return res.status(err.statusCode).json({ message: err.message });
-  }
-  return res.status(500).json({ message: "Something went wrong, please try again" });
+const notFound = (req, res) => {
+  return res.status(404).json({ message: "Route does not exist" });
 }
 
-module.exports = errorHandlerMiddleware;
+module.exports = notFound;
