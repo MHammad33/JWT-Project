a6c563065d7726397eebfaadffd21a3cf64b0709
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-26 22:28:57 +0500

    Add different Error handling classes

---
+++ src/errors/index.js
@@ -0,0 +1,9 @@
+const CustomApiError = require("./custom-error");
+const BadRequest = require("./bad-request");
+const UnauthenticatedError = require("./unauthenticated");
+
+module.exports = {
+  CustomApiError,
+  BadRequest,
+  UnauthenticatedError
+};
